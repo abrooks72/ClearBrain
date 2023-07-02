46ffeeeec911ecd8cd91c2a58c5e79140378948e
Austin Brooks <austin.williambrooks@gmail.com>
2023-07-02 15:42:49 -0800

    Settings screen has work to be done on sliders. Made an additional help screen off of settings screen with contact info

---
+++ Assets/Scripts/BrightnessController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrightnessController : MonoBehaviour
+{
+
+    public Slider slider;
+    public Light sceneLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sceneLight.intensity = slider.value;
+    }
+}
